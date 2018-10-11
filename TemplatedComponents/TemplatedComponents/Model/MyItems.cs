02ce8da6d4e502ad35e8ca48dd206dfaac0f452d
Jignesh Trivedi <jignesht24@gmail.com>
2018-10-11 18:13:27 +0530

    init checked-in

---
+++ TemplatedComponents/TemplatedComponents/Model/MyItems.cs
@@ -0,0 +1,9 @@
+﻿namespace TemplatedComponents.Model
+{
+    public class MyItems
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
